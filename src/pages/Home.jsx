93135d4c8231546cee3d14774f2a8b7e97657d6e
lumiela <ssler1@naver.com>
2025-12-18 14:30:09 +0900

    프로젝트명 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="hero-content">
         <h1 className="hero-title">안녕하세요, 환영합니다!</h1>
         <p className="hero-subtitle">
-          저는 현대적이고 반응형 웹 애플리케이션을 만드는 것을 즐기는 개발자입니다.
+          개발자입니다
           <br />
           제 프로젝트들을 둘러보시고 저에 대해 더 알아보세요.
         </p>
