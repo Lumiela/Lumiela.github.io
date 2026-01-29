4165fad35f1906c16174606f2a1bd7691b76f360
lumiela <ssler1@naver.com>
2026-01-29 10:49:40 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cnc-calc/sw.js', { scope: '/cnc-calc/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
